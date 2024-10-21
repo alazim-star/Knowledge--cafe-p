da65c59273182f6dff9a64b98e9c3d11502d86e5
unknown <alazim-star>
2024-10-22 01:58:16 +0600

    add Blogs for data load

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './Component/Header/Header'
+import Blogs from './Component/Blogs/Blogs';
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
+return (
     <>
-     
-      <h1 className='text-6xl bg-red-200'>knowledge Cafe</h1>
-     
+     <Header></Header>
+     <Blogs></Blogs>
+    
     </>
   )
 }
